bb70841f40ccd2ee7875d02d1291f81160e103fd
Viet Hung Nguyen <hvn@familug.org>
2017-12-19 22:03:34 +0700

    Bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requisites = []
 
 setup(
     name='phg',
-    version='1.1.0',
+    version='2.0.0',
     description='Password hasher and generator',
     scripts=['scripts/phg'],
     long_description=open('README.rst').read(),

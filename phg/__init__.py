260ee1e23c3432dc2fd12d0f63de3664a659a4d3
Hung Nguyen Viet <hvn@familug.org>
2017-12-19 12:55:26 +0700

    Default to generate password comply to AWS policy

--- phg/__init__.py
+++ phg/__init__.py
@@ -2,19 +2,34 @@ import random
 import string
 
 
-PUNC_NO_QUOTE = string.punctuation.replace('"', '').replace("'", "")
 DEFAULT_LENGTH = 16
 
 
-def random_password(length=DEFAULT_LENGTH, with_quote=False):
-    punc = string.punctuation if with_quote else PUNC_NO_QUOTE
+# http://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_passwords_account-policy.html#password-policy-details
+symbols = "!@#$%^&*()_+-=[]{}|'"
+
+
+def random_password(length=DEFAULT_LENGTH, with_quote=False, symbol=True):
+    '''
+    Generate random password - NOT CRYPTOGRAPHICALLY SAFE
+    (see Python3.6 secret module).
+
+    symbol: use AWS symbol characters instead of punctuations (some char diff).
+    with_quote: password might contain quotes
+    '''
+    punc = string.punctuation
+    if symbol:
+        punc = symbols
+
+    if not with_quote:
+        punc = punc.replace('"', '').replace("'", "")
 
     result = []
     result.append(random.choice(string.ascii_lowercase))
     result.append(random.choice(string.ascii_uppercase))
     result.append(random.choice(string.digits))
     result.append(random.choice(punc))
-    for i in range(length-len(result)):
+    for i in range(length - len(result)):
         result.append(random.choice(
             string.ascii_letters +
             string.digits + punc))
